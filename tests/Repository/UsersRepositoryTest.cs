5a1eed2b4b5394d5b1b1c302d7f27e0631da7a20
Luis E. Rondon <leduardo.rondon@gmail.com>
2023-11-15 21:50:36 -0400

    Add test respostory

--- tests/Repository/UsersRepositoryTest.cs
+++ tests/Repository/UsersRepositoryTest.cs
@@ -1,47 +1,62 @@
-using System.Collections;
-using System.Data;
+using Xunit;
+using Moq;
 using dotnetcaching.Entities;
 using dotnetcaching.Repository;
-using dotnetcaching.Utils;
-using Moq;
-using Xunit;
 
 public class UsersRepositoryTest
 {
-    private Mock<CustomDataTable> _dbConMock;
-    private DataTable _dt;
-    private Hashtable _params;
-    private UsersRepository _usersRepository;
+    private readonly Mock<IUsersRepository> _mockRepo;
 
-    [Fact]
-    public void Setup()
+    public UsersRepositoryTest()
     {
-        _dbConMock = new Mock<CustomDataTable>();
-        _dt = new DataTable();
-        _params = new Hashtable();
-        _usersRepository = new UsersRepository();
-
-        // Aquí puedes agregar la configuración de tus objetos simulados (_dbConMock)
-    }
-    private static async Task<UsersRepository> SetupMockUsersRepository(List<UsersRolPolicy> usersRolPolicies)
-    {
-        return new UsersRepository();
+        _mockRepo = new Mock<IUsersRepository>();
     }
 
     [Fact]
-    public async Task SelectPolicyUsersAllRepository_ReturnsExpectedResult()
+    public async Task SelectPolicyUsersAllRepository_ReturnsListOfUsersRolPolicy()
     {
         // Arrange
-        var mockDbCon = new Mock<IDbConnection>();
-        var mockDt = new DataTable();
-        // Add rows to mockDt as needed
-        IUsersRepository mockHolidayRepository = await SetupMockUsersRepository(new List<UsersRolPolicy> { });
-        //mockDbCon.Setup(db => db.ExecuteAsync(It.IsAny<string>(), It.IsAny<object>())).ReturnsAsync(mockDt);
+        var users = new List<UsersRolPolicy>
+                {
+                    new UsersRolPolicy { /* inicializa con datos de prueba */ },
+                    new UsersRolPolicy { /* inicializa con datos de prueba */ }
+                };
+        _mockRepo.Setup(repo => repo.SelectPolicyUsersAllRepository()).ReturnsAsync(users);
 
         // Act
-        var result = await SetupMockUsersRepository(new List<UsersRolPolicy> { });
-        var users = result.SelectPolicyUsersAllRepository().Result;
+        var result = await _mockRepo.Object.SelectPolicyUsersAllRepository();
+
         // Assert
-        Assert.Equal(0, users.Count);
+        Assert.Equal(users, result);
+    }
+
+    [Fact]
+    public async Task SelectPolicyUsersByIdRepository_ReturnsOfUsersRolPolicy()
+    {
+        // Arrange
+        int userId = 1;
+        var users = new UsersRolPolicy
+                { };
+        _mockRepo.Setup(repo => repo.SelectPolicyUserByIdRepository(userId)).ReturnsAsync(users);
+
+        // Act
+        var result = await _mockRepo.Object.SelectPolicyUserByIdRepository(userId);
+
+        // Assert
+        Assert.Equal(users, result);
+    }
+
+    [Fact]
+    public async Task InsertPolicyUsersRepository_ReturnsIdUser()
+    {
+        // Arrange
+        var usersDto = new UserDto {};
+        _mockRepo.Setup(repo => repo.InsertUserRepository(usersDto)).ReturnsAsync(1);
+
+        // Act
+        var result = await _mockRepo.Object.InsertUserRepository(usersDto);
+
+        // Assert
+        Assert.Equal(1, result);
     }
 }
